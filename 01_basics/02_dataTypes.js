6eac3f408025d111798a5b85b31032517e37e1d6
Vaishnavi Kulkarni <124790848+vaishkul1@users.noreply.github.com>
2025-05-18 11:22:31 +0000

    Day2

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -4,10 +4,7 @@
 let state = null
 
 /*
-number
-big int
-string
-boolean
+number, big int, string, boolean
 null: standalone value. it is not undefined it is "Null"()Empty!
 undefined
 symbol: unique
@@ -15,3 +12,40 @@ symbol: unique
 
 console.log(typeof null);   // o/p-> object
 console.log(typeof undefined);  //o/p -> undefined
+//******************************* 
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
